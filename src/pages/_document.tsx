d55f7c8a43be8c9834c76b77dafe599e72147b7f
floriansr <sueurfloriansf@gmail.com>
2020-11-22 19:15:40 +0100

    [add] seal stylesheets

---
+++ src/pages/_document.tsx
@@ -0,0 +1,52 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document<any> {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // useful for wrapping the whole react tree
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      // useful for custom metadata
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default MyDocument;
