6186cfb10af5ed1bfa553cb0fb99f2afdb564df7
floriansr <sueurfloriansf@gmail.com>
2020-11-23 14:56:14 +0100

    [add] next.config.js allows support of importing images files and fonts within webpack.

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+const withFonts = require('next-fonts');
+
+dotenv.config();
+
+module.exports = withPlugins([withFonts, withImages], {
+  onDemandEntries: {
+    // Make sure entries are not getting disposed.
+    maxInactiveAge: 1000 * 60 * 60
+  },
+  webpack(config) {
+    config.node = {
+      fs: 'empty'
+    };
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __DEV__: JSON.stringify(process.env.NODE_ENV === 'development')
+      })
+    );
+
+    return config;
+  }
+});
