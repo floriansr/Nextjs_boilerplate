721287616f4f9ee253438f0bbbf0cd16ae6bc9a1
floriansr <sueurfloriansf@gmail.com>
2020-11-24 17:15:03 +0100

    [add] modules manager import

---
+++ src/styles/Modules/index.ts
@@ -0,0 +1,5 @@
+import colors from 'styles/Modules/Colors';
+import reset from 'styles/Modules/Reset';
+import { MAIN_BACKGROUND } from 'styles/Modules/Variables';
+
+export { reset, colors, MAIN_BACKGROUND };
