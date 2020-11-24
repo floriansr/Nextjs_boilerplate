ab6d83a8a3cdd6e9186d602485f3ba0b33005bcc
floriansr <sueurfloriansf@gmail.com>
2020-11-24 17:18:35 +0100

    [add] typography manager import

---
+++ src/ui/Typography/index.tsx
@@ -0,0 +1,5 @@
+import AppLink from 'ui/Typography/AppLink';
+import Heading, { HeadingSize } from 'ui/Typography/Heading';
+import Text from 'ui/Typography/Text';
+
+export { Heading, HeadingSize, Text, AppLink };
