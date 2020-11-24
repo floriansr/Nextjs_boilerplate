3de75e5789cdd887b47cb2709e60dda854a6cf95
floriansr <sueurfloriansf@gmail.com>
2020-11-24 17:21:33 +0100

    [add] light theme

---
+++ src/styles/Themes/Light/index.ts
@@ -0,0 +1,18 @@
+import type { DefaultTheme } from 'styled-components';
+import { colors } from 'styles/Modules';
+import baseTheme from 'styles/Themes/Base';
+
+const theme: DefaultTheme = {
+  ...baseTheme,
+  colors: {
+    ...baseTheme.colors,
+    background: colors.white,
+    text: colors.dark,
+    link: colors.blue[600],
+    primary: colors.orange[600],
+    secondary: colors.purple[600],
+    tertiary: colors.green[600]
+  }
+};
+
+export default theme;
