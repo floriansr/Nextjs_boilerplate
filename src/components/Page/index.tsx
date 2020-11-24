3cc5d6a8c779f57de85965df2f692f3c03de989d
floriansr <sueurfloriansf@gmail.com>
2020-11-24 17:25:42 +0100

    [add] Page component for framer-motion

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import { motion } from 'framer-motion';
+import { NextPage } from 'next';
+import styled from 'styled-components';
+import { BREAKPOINTS } from 'styles/Modules/Variables';
+
+import { DefaultRouterPageVariants } from 'tools/Framer-Motion';
+
+interface Props {
+  readonly as?: string;
+  readonly noDefaultTransition?: boolean;
+}
+
+const PageInner = styled(motion.div)`
+  flex: 1;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  transform-origin: left center;
+  max-width: 90vw;
+  margin: 0 auto;
+  @media screen and (min-width: ${BREAKPOINTS.tablet}) {
+    min-width: 70vw;
+  }
+`;
+
+const Page: NextPage<Props> = ({ children, as = 'div', noDefaultTransition = false, ...rest }) =>
+  React.createElement(
+    // @ts-ignore
+    motion[as],
+    {
+      exit: 'exit',
+      initial: 'exit',
+      animate: 'enter',
+      className: 'router__wrapper'
+    },
+    <PageInner {...rest} {...(noDefaultTransition ? {} : { variants: DefaultRouterPageVariants })}>
+      {children}
+    </PageInner>
+  );
+
+export default Page;
