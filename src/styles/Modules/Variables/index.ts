b157edda87bd923346b9a7ed0623885e3308bcf4
floriansr <sueurfloriansf@gmail.com>
2020-11-26 12:15:54 +0100

    [check] background- color props

--- src/styles/Modules/Variables/index.ts
+++ src/styles/Modules/Variables/index.ts
@@ -9,5 +9,5 @@ export const BREAKPOINTS: Breakpoints = breakpoints;
 export const ELASTIC_BEZIER = 'cubic-bezier(0.32, 2, 0.55, 0.27)';
 
 export const MAIN_BACKGROUND = css`
-  background: ${(props) => theme(props).colors.background};
+  background: ${(props): string => theme(props).colors.background};
 `;
