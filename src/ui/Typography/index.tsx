2e15fe0e1d62ff8eaa5088754cac55ee480770a1
floriansr <sueurfloriansf@gmail.com>
2020-11-29 18:37:49 +0100

    [erase] file

--- src/ui/Typography/index.tsx
+++
@@ -1,5 +0,0 @@
-import AppLink from 'ui/Typography/AppLink';
-import Heading, { HeadingSize } from 'ui/Typography/Heading';
-import Text from 'ui/Typography/Text';
-
-export { Heading, HeadingSize, Text, AppLink };
